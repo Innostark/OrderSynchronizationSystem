e9170abfc66829003e23d9a8437c376f82f87a02
quantumbr <bilal@innostark.com>
2014-08-09 18:17:43 +0100

    Service Call Made

--- IST.OrderSynchronizationSystem/IST.OrderSynchronizationSystem/Program.cs
+++ IST.OrderSynchronizationSystem/IST.OrderSynchronizationSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using IST.OrderSynchronizationSystem.MBAPI;
 
 
 namespace IST.OrderSynchronizationSystem
@@ -12,6 +13,17 @@ namespace IST.OrderSynchronizationSystem
         [STAThread]
         static void Main()
         {
+            var MoldingBoxSoapClient = new MBAPI.MBAPISoapClient();
+
+            MoldingBoxSoapClient.ClientCredentials.UserName.UserName = "SKU1";
+            MoldingBoxSoapClient.ClientCredentials.UserName.Password = "MBLLTAtOr8wKNI2Z44oPj2s81Hea6F";
+
+            var GetShipingMethods = new MBAPI.Retrieve_Shipping_MethodsRequest();
+            var ShipingMethodsResponse = new MBAPI.Retrieve_Shipping_MethodsResponse();
+
+            //GetShipingMethods.Body = new Retrieve_Shipping_MethodsRequestBody();
+            var a123 = MoldingBoxSoapClient.Retrieve_Shipping_Methods();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
