9b2a23c21ffaeea2ba5c735827de43581b861188
Mohammad Ali Raza <ali@innostark.com>
2014-08-12 19:02:42 +0500

    Order Loading from THub db and shipment creation completed

--- IST.OrderSynchronizationSystem/IST.OrderSynchronizationSystem/OSSDatabase.cs
+++ IST.OrderSynchronizationSystem/IST.OrderSynchronizationSystem/OSSDatabase.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
+using System.Transactions;
+using IST.OrderSynchronizationSystem.MBAPI;
 
 namespace IST.OrderSynchronizationSystem
 {
@@ -34,7 +39,7 @@ namespace IST.OrderSynchronizationSystem
             {
                 using (SqlConnection connection = new SqlConnection(sourceSqlConnectionConnectionStringBuilder.ConnectionString))
                 {
-                    using (SqlCommand command = new SqlCommand("Select count(*) from Orders", connection))
+                    using (SqlCommand command = new SqlCommand(SqlResource.source_sql_verify, connection))
                     {
                         connection.Open();
                         command.ExecuteNonQuery();
@@ -53,7 +58,7 @@ namespace IST.OrderSynchronizationSystem
             {
                 using (SqlConnection connection = new SqlConnection(stagingSqlConnectionConnectionStringBuilder.ConnectionString))
                 {
-                    using (SqlCommand command = new SqlCommand("Select count(*) from OSSOrders", connection))
+                    using (SqlCommand command = new SqlCommand(SqlResource.staging_sql_verify, connection))
                     {
                         connection.Open();
                         command.ExecuteNonQuery();
@@ -63,10 +68,74 @@ namespace IST.OrderSynchronizationSystem
             }
             catch (Exception)
             {
-
                 return false;
             }
         }
 
+        public List<Shipment> LoadOrdersAndCreateShipments()
+        {
+            try
+            {
+                List<Shipment> shipments = new List<Shipment>();
+                using (TransactionScope scope = new TransactionScope())
+                {
+                    //TODO: Use in where clause
+                    DateTime lastExecutionTime;
+                    using (SqlConnection stagingDbconnection = new SqlConnection(stagingSqlConnectionConnectionStringBuilder.ConnectionString))
+                    {
+                        stagingDbconnection.Open();
+                        using (SqlCommand command = new SqlCommand(SqlResource.staging_sql_LastExecutionTimeQuery, stagingDbconnection))
+                        {
+                            var result = command.ExecuteScalar();
+                            lastExecutionTime = result == null ? DateTime.Now.Date : DateTime.Parse(string.Format(CultureInfo.InvariantCulture, result.ToString()));
+                        }
+
+                        
+                    }
+                    using (SqlConnection tHubDbConnection = new SqlConnection())
+                    {
+                        using (SqlCommand command = new SqlCommand(SqlResource.staging_sql_PullOrdersFromThubQuery, tHubDbConnection))
+                        {
+                            SqlDataReader results = command.ExecuteReader();
+                            while (results.Read())
+                            {
+                                AddRowsToShipmentList(shipments, results);
+                            }
+                        }
+                    }
+                }
+
+                return shipments;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        private static void AddRowsToShipmentList(List<Shipment> shipments, IDataRecord record)
+        {
+            Shipment shipmentFromDatabase = new Shipment
+            {
+                OrderID = record["OrderID"].ToString(),
+                Orderdate = (DateTime)record["Orderdate"],
+                Company = record["Company"].ToString(),
+                FirstName = record["FirstName"].ToString(),
+                LastName = record["LastName"].ToString(),
+                Address1 = record["Address1"].ToString(),
+                Address2 = record["Address2"].ToString(),
+                City = record["City"].ToString(),
+                State = record["State"].ToString(),
+                Country = record["Country"].ToString(),
+                Email = record["Email"].ToString(),
+                Phone = record["Phone"].ToString(),
+                ShippingMethodID = (int)record["ShippingMethodID"],
+                Custom1 = record["Custom1"].ToString(),
+                Custom2 = record["Custom2"].ToString(),
+                Custom3 = record["Custom3"].ToString(),
+                Custom4 = record["Custom4"].ToString(),
+                Custom5 = record["Custom5"].ToString(),
+            };
+            shipments.Add(shipmentFromDatabase);
+        }
     }
 }
